c46d9eed1dadce64046da5fcf13f31e7ec96c24d
NoxXxz <noxxxz09@gmail.com>
2024-06-26 15:17:04 -0400

    starting littleLemon App

---
+++ SwLittleLemonApp/SwLittleLemonApp/SwLittleLemonAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwLittleLemonAppApp.swift
+//  SwLittleLemonApp
+//
+//  Created by Arturo Camacho on 26/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwLittleLemonAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
